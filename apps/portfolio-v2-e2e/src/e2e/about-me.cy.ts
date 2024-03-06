662d87fb745c92084574df877b673a8931bffe04
PubuduS <pubudusupun@gmail.com>
2024-03-05 22:20:19 -0600

    Use Fixtures.
    Use fixtures instead of hard-coded data.

--- apps/portfolio-v2-e2e/src/e2e/about-me.cy.ts
+++ apps/portfolio-v2-e2e/src/e2e/about-me.cy.ts
@@ -1,11 +1,3 @@
-const param1 = 'My name is Pubudu. Currently, I live in Omaha, Nebraska. I enjoy building software, games, and websites. My interest in software engineering started back in 2012 when I decided to attend a code camp. After that, I decided to pursue my higher education in the US.';
-
-const param2 = 'Fast forward to today, I\'ve had the privilege of working at A corporation, a charitable organization, and a small business. I graduated from the University of Omaha at Nebraska with a MSc in Computer Science in May 2023. These days I am actively looking forward jobs that align with my skills.';
-
-const param3 = 'I am currently open to new opportunities where I can learn new technologies and helped to develop robust software. Need something built or simply want to have chat? Reach out to me on social media or send me an email.';
-
-const param4 = 'When I\'m not coding my next project, I enjoy spending my time doing any on the following:';
-
 describe('about-me page', () => {
   beforeEach(() => cy.visit('/aboutme'));
 
@@ -21,16 +13,22 @@ describe('about-me page', () => {
     cy.get('h2').should('have.text', 'ABOUT ME')
   })
 
-  it('should display first introductory paragraph', () => {
-    cy.get('p').eq(0).should('have.text', param1)
+  it('should display first introductory paragraph', () => {    
+    cy.fixture('about-me/intro-paras.json').then((intro) => {
+        cy.get('p').eq(0).should('have.text', intro.paragraph0)
+    })    
   })
 
   it('should display second introductory paragraph', () => {
-    cy.get('p').eq(1).should('have.text', param2)
+    cy.fixture('about-me/intro-paras.json').then((intro) => {
+        cy.get('p').eq(1).should('have.text', intro.paragraph1)
+    })    
   })
 
   it('should display third introductory paragraph', () => {
-    cy.get('p').eq(2).should('have.text', param3)
+    cy.fixture('about-me/intro-paras.json').then((intro) => {
+        cy.get('p').eq(2).should('have.text', intro.paragraph2)
+    })    
   })
 
   it('should display interest title', () => {
@@ -38,28 +36,40 @@ describe('about-me page', () => {
   })
 
   it('should display third introductory paragraph', () => {
-    cy.get('p').eq(3).should('have.text', param4)
+    cy.fixture('about-me/interests.json').then((interests) => {
+        cy.get('p').eq(3).should('have.text', interests.intro)
+    })  
   })
 
   // li 1 - 8 are for menu bar.
   it('should display interest 1', () => {
-    cy.get('li').eq(9).should('have.text', 'I love Cooking. I love to create Asian American fusion dishes.')
+    cy.fixture('about-me/interests.json').then((interests) => {
+        cy.get('li').eq(9).should('have.text', interests.interest1)
+    })  
   })
 
-  it('should display interest 2', () => {
-    cy.get('li').eq(10).should('have.text', 'I\'m a bookworm and fan of Philosophy, Science Fiction, Fictional, and Mystery genre.')
+  it('should display interest 2', () => {   
+    cy.fixture('about-me/interests.json').then((interests) => {
+        cy.get('li').eq(10).should('have.text', interests.interest2)
+    })  
   })
 
-  it('should display interest 3', () => {
-    cy.get('li').eq(11).should('have.text', 'I\'m an avid fan of video games. Currently, I\'m playing Apex Legends. If you\'re interested you can see my gameplays here.')
+  it('should display interest 3', () => {    
+    cy.fixture('about-me/interests.json').then((interests) => {
+        cy.get('li').eq(11).should('have.text', interests.interest3)
+    })  
   })
 
-  it('should display interest 4', () => {
-    cy.get('li').eq(12).should('have.text', 'I\'m eager to learn new technologies and new skills.')
+  it('should display interest 4', () => {    
+    cy.fixture('about-me/interests.json').then((interests) => {
+        cy.get('li').eq(12).should('have.text', interests.interest4)
+    })  
   })
 
-  it('should display interest 5', () => {
-    cy.get('li').eq(13).should('have.text', 'I enjoy playing Chess.')
+  it('should display interest 5', () => {    
+    cy.fixture('about-me/interests.json').then((interests) => {
+        cy.get('li').eq(13).should('have.text', interests.interest5)
+    })  
   })
 
 });
