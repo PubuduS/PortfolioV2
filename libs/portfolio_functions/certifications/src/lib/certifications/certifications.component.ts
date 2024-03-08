5495f048becb5df057d21b21a5224c08d1a156fc
PubuduS <pubudusupun@gmail.com>
2024-03-07 18:24:08 -0600

    Simplified some code in certificates section.

--- libs/portfolio_functions/certifications/src/lib/certifications/certifications.component.ts
+++ libs/portfolio_functions/certifications/src/lib/certifications/certifications.component.ts
@@ -13,60 +13,70 @@ export class CertificationsComponent {
   public readonly certificate_icon_URL: string = 'assets/images/certificate_Icon/certificate.png';
   public readonly tooltip: string = 'Click here to view the certificate';
 
-  public readonly cloudPractitioner: ICertificateCard = {
-    title: 'AWS Certified Cloud Practitioner',
-    completedDate: 'In progress',
-    organization: 'Udemy',
-    credentialID: 'None',
-    certification: 'In Progress',
-    description: 'This is a introductory level course about AWS.',
-  }
+  public readonly certificatesLeft: ICertificateCard[] = [
+    {
+      title: 'Angular Level 1 Certification',
+      completedDate: new Date(2024, 1, 21).toLocaleDateString(),
+      organization: 'Angular Training',
+      credentialID: '5A20CF0',
+      certification: 'Here',
+      certificateURL: 'https://interstate21.com/certificate/?code=5A20CF0',
+      description: 'This is a professional certification from by Angular Training',
+    },
+    {
+      title: 'AWS Certified Cloud Practitioner',
+      completedDate: 'In progress',
+      organization: 'Udemy',
+      credentialID: 'None',
+      certification: 'In Progress',
+      description: 'This is a introductory level course about AWS.',      
+    },
+    {
+      title: 'Object Oriented Programming in Java',
+      completedDate: new Date(2020, 1, 9).toLocaleDateString(),
+      organization: 'Coursera',
+      credentialID: '64EPBBAFEWYL',
+      certification: 'Here',
+      certificateURL: 'https://www.coursera.org/account/accomplishments/certificate/64EPBBAFEWYL',
+      description: 'This is a online Coursera course offered by University of California San Diego.',
+    },
+    {
+      title: 'Web Development Bootcamp',
+      completedDate: 'In progress',
+      organization: 'Udemy',
+      credentialID: 'None',
+      certification: 'In Progress',
+      description: 'This is a introductory level course about web development.',
+    }
+  ]
 
-  public readonly multiThread: ICertificateCard = {
-    title: 'Modern C++ Multi-threading',
-    completedDate: new Date(2023, 3, 26).toLocaleDateString(),
-    organization: 'Udemy',
-    credentialID: 'e2be974d7844',
-    certification: 'Here',
-    certificateURL: 'https://www.udemy.com/certificate/UC-ecab9d2a-2f1c-4eab-856c-e2be974d7844/',
-    description: 'This is an introductory course on modern C++ multi-threading capabilities.',
-  }
+  public readonly certificatesRight: ICertificateCard[] = [
 
-  public readonly oopJava: ICertificateCard = {
-    title: 'Object Oriented Programming in Java',
-    completedDate: new Date(2020, 1, 9).toLocaleDateString(),
-    organization: 'Coursera',
-    credentialID: '64EPBBAFEWYL',
-    certification: 'Here',
-    certificateURL: 'https://www.coursera.org/account/accomplishments/certificate/64EPBBAFEWYL',
-    description: 'This is a online Coursera course offered by University of California San Diego.',
-  }
-
-  public readonly deepLearning: ICertificateCard = {
-    title: 'Neural Network and Deep Learning',
-    completedDate: new Date(2019, 7, 9).toLocaleDateString(),
-    organization: 'Coursera',
-    credentialID: 'R7P6PNDABS9V',
-    certification: 'Here',
-    certificateURL: 'https://www.coursera.org/account/accomplishments/certificate/R7P6PNDABS9V',
-    description: 'This is a introductory level course about deep learning.',
-  }
-
-  public readonly unityGame: ICertificateCard = {
-    title: 'Game Development with Unity',
-    completedDate: 'In progress',
-    organization: 'Udemy',
-    credentialID: 'None',
-    certification: 'In Progress',
-    description: 'This is a introductory level course about 2D game development with C#.',
-  }
-
-  public readonly webDev: ICertificateCard = {
-    title: 'Web Development Bootcamp',
-    completedDate: 'In progress',
-    organization: 'Udemy',
-    credentialID: 'None',
-    certification: 'In Progress',
-    description: 'This is a introductory level course about web development.',
-  }
+    {
+      title: 'Modern C++ Multi-threading',
+      completedDate: new Date(2023, 3, 26).toLocaleDateString(),
+      organization: 'Udemy',
+      credentialID: 'e2be974d7844',
+      certification: 'Here',
+      certificateURL: 'https://www.udemy.com/certificate/UC-ecab9d2a-2f1c-4eab-856c-e2be974d7844/',
+      description: 'This is an course on modern C++ multi-threading.',
+    },
+    {
+      title: 'Game Development with Unity',
+      completedDate: 'In progress',
+      organization: 'Udemy',
+      credentialID: 'None',
+      certification: 'In Progress',
+      description: 'This is a course about 2D game development with C#.',
+    },
+    {
+      title: 'Neural Network and Deep Learning',
+      completedDate: new Date(2019, 7, 9).toLocaleDateString(),
+      organization: 'Coursera',
+      credentialID: 'R7P6PNDABS9V',
+      certification: 'Here',
+      certificateURL: 'https://www.coursera.org/account/accomplishments/certificate/R7P6PNDABS9V',
+      description: 'This is a introductory level course about deep learning.',
+    }
+  ]
 }
