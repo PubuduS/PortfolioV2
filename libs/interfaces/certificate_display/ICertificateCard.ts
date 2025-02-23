85ef471ea21352efcf90bc0f04ca44c0b964afe8
PubuduS <pubudusupun@gmail.com>
2025-02-22 20:27:23 -0600

    Add Firebase support to the certificates section
    Refactor CSS to SCSS

--- libs/interfaces/certificate_display/ICertificateCard.ts
+++ libs/interfaces/certificate_display/ICertificateCard.ts
@@ -1,10 +1,20 @@
+/** Interface for Certificates */
 export interface ICertificateCard
 {
+    /** ID */
+    id: number,
+    /** Certificate Title */
     title: string;
-    completedDate: Date | string;
+    /** Completion Date */
+    completedDate: string;
+    /** Issuing Organization */
     organization: string;
+    /** Credential ID. Use None if unavailable */
     credentialID: string;
+    /** Certification State */
     certification: 'Here' | 'In Progress'
+    /** Certification URL */
     certificateURL?: string;
+    /** Description */
     description: string;
 }
--- libs/pages/src/lib/components/education_main/components/certifications/certifications.component.ts
+++ libs/pages/src/lib/components/education_main/components/certifications/certifications.component.ts
@@ -3,8 +3,10 @@ import {
   Component,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 
 import { ICertificateCard } from '@portfolio-v2/interfaces';
+import { GetDataService } from '@portfolio-v2/shared/services';
 
 /**
  * Certifications Page
@@ -18,73 +20,18 @@ import { ICertificateCard } from '@portfolio-v2/interfaces';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CertificationsComponent {
+  /** Certificates Icon URL */
   public readonly certificate_icon_URL: string = 'assets/images/certificate_Icon/certificate.png';
+  /** Tooltip string */
   public readonly tooltip: string = 'Click here to view the certificate';
+  /** Observable of ICertificateCard array */
+  protected certificatesCards$: Observable<ICertificateCard[]>;
 
-  public readonly certificatesLeft: ICertificateCard[] = [
-    {
-      title: 'Angular Level 1 Certification',
-      completedDate: new Date(2024, 1, 21).toLocaleDateString(),
-      organization: 'Angular Training',
-      credentialID: '5A20CF0',
-      certification: 'Here',
-      certificateURL: 'https://interstate21.com/certificate/?code=5A20CF0',
-      description: 'This is a professional certification from by Angular Training',
-    },
-    {
-      title: 'AWS Certified Cloud Practitioner',
-      completedDate: 'In progress',
-      organization: 'Udemy',
-      credentialID: 'None',
-      certification: 'In Progress',
-      description: 'This is a introductory level course about AWS.',
-    },
-    {
-      title: 'Object Oriented Programming in Java',
-      completedDate: new Date(2020, 1, 9).toLocaleDateString(),
-      organization: 'Coursera',
-      credentialID: '64EPBBAFEWYL',
-      certification: 'Here',
-      certificateURL: 'https://www.coursera.org/account/accomplishments/certificate/64EPBBAFEWYL',
-      description: 'This is a online Coursera course offered by University of California San Diego.',
-    },
-    {
-      title: 'Web Development Bootcamp',
-      completedDate: 'In progress',
-      organization: 'Udemy',
-      credentialID: 'None',
-      certification: 'In Progress',
-      description: 'This is a introductory level course about web development.',
-    },
-  ];
-
-  public readonly certificatesRight: ICertificateCard[] = [
-
-    {
-      title: 'Modern C++ Multi-threading',
-      completedDate: new Date(2023, 3, 26).toLocaleDateString(),
-      organization: 'Udemy',
-      credentialID: 'e2be974d7844',
-      certification: 'Here',
-      certificateURL: 'https://www.udemy.com/certificate/UC-ecab9d2a-2f1c-4eab-856c-e2be974d7844/',
-      description: 'This is an course on modern C++ multi-threading.',
-    },
-    {
-      title: 'Game Development with Unity',
-      completedDate: 'In progress',
-      organization: 'Udemy',
-      credentialID: 'None',
-      certification: 'In Progress',
-      description: 'This is a course about 2D game development with C#.',
-    },
-    {
-      title: 'Neural Network and Deep Learning',
-      completedDate: new Date(2019, 7, 9).toLocaleDateString(),
-      organization: 'Coursera',
-      credentialID: 'R7P6PNDABS9V',
-      certification: 'Here',
-      certificateURL: 'https://www.coursera.org/account/accomplishments/certificate/R7P6PNDABS9V',
-      description: 'This is a introductory level course about deep learning.',
-    },
-  ];
+  /**
+   * constructor
+   * @param dataService data service
+   */
+  constructor(private dataService: GetDataService) {
+    this.certificatesCards$ = this.dataService.getCertificatesInformation('certification-section');
+  }
 }
