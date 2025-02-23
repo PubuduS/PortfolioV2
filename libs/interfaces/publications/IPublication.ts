bf3190cdc3b862c2769929680406ef7ce89b874e
PubuduS <pubudusupun@gmail.com>
2025-02-22 22:54:04 -0600

    Add Firebase support to publications section

--- libs/interfaces/publications/IPublication.ts
+++ libs/interfaces/publications/IPublication.ts
@@ -1,17 +1,35 @@
+/** Interface for Publications */
 export interface IPublication {
-    title: string,
-    subTitle: string,
-    imageUrl: string,
-    shortDesc: string,
-    btnMoreDisable: boolean,    
-    btnDownloadDisable: boolean,
-    downloadUrl?: string,
-    downloadFileName: string
+  /** ID */
+  id: number,
+  /** Title */
+  title: string,
+  /** Subtitle */
+  subTitle: string,
+  /** Image URL */
+  imageUrl: string,
+  /** Short Description */
+  shortDesc: string,
+  /** More Button Disable */
+  btnMoreDisable: boolean,
+  /** Download Button Disable */
+  btnDownloadDisable: boolean,
+  /** Download URL */
+  downloadUrl?: string,
+  /** Download File Name */
+  downloadFileName: string
 }
 
+/** Interface for Publication Details */
 export interface IPublicationDetails {
-    title: string,
-    authors: string,
-    abstarct: string,
-    citation: string
+  /** ID */
+  id: number,
+  /** Title */
+  title: string,
+  /** Authors */
+  authors: string,
+  /** Abstarct */
+  abstarct: string,
+  /** Citation */
+  citation: string
 }
