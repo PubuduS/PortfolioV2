24a731252fa8449182d8404e582f2f9e79822504
PubuduS <pubudusupun@gmail.com>
2025-08-02 14:55:47 -0500

    Add security enhancements

--- apps/portfolio-v2-e2e/src/support/commands.ts
+++ apps/portfolio-v2-e2e/src/support/commands.ts
@@ -19,11 +19,6 @@ declare namespace Cypress {
   }
 }
 
-// -- This is a parent command --
-Cypress.Commands.add('login', (email, password) => {
-  console.log('Custom command example: Login', email, password);
-});
-
 Cypress.Commands.add('navbar', () => {
   cy.contains('Home');
   cy.contains('About Me');
@@ -35,14 +30,3 @@ Cypress.Commands.add('navbar', () => {
   cy.contains('Cover Letter');
   cy.get('[data-contact-btn="Contact"]');
 });
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
